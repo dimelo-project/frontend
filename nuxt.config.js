3baf1c147275e95d1e29c2d36523ab8aa0db475d
DESKTOP-P1UNOTJ\jason <1127lse@gmail.com>
2022-01-04 19:45:23 +0900

    setting: reset.scss 생성 & global css에 등록(#4)

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ["~assets/scss/_variables.scss"],
+  css: ["./assets/scss/reset.scss"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
