c8595fcd153a26ea726ecbcf7aec3c7d4f9f6624
DESKTOP-P1UNOTJ\jason <1127lse@gmail.com>
2022-01-06 09:51:37 +0900

    setting: ref용 폴더 생성 & 기존 예시 파일 이동(#8)

---
+++ components/ref/button/category.vue
@@ -0,0 +1,19 @@
+<template>
+  <nuxt-link :to="refUrl">
+    <button
+      type="button"
+      class="px-4 py-1 mt-1 ml-1 border border-gray-800 rounded-full hover:ring-2 hover:ring-blue-700 hover:bg-gray-100 hover:text-blue-700"
+    >
+      {{ refTopic }}
+    </button>
+  </nuxt-link>
+</template>
+
+<script>
+export default {
+  props: {
+    refUrl: String,
+    refTopic: String,
+  },
+};
+</script>
---
+++ pages/ref/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <RefButtonCategory refUrl="ref/sass" refTopic="sass" />
+    <RefButtonCategory refUrl="ref/tailwind" refTopic="tailwind example" />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
