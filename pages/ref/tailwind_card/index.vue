e383b315d78637c4c7e090dd20e5c5fa00f25544
DESKTOP-P1UNOTJ\jason <1127lse@gmail.com>
2022-01-10 20:25:35 +0900

    setting: 디자인가이드에 따른 font 클래스들 생성, 예시파일작성 & 기존파일들에 적용(#18)

--- pages/ref/tailwind_card/index.vue
+++ pages/ref/tailwind_card/index.vue
@@ -1,22 +1,22 @@
 <template>
   <div class="p-4">
-    <div class="max-w-sm rounded overflow-hidden shadow-lg mb-4">
+    <div class="max-w-sm mb-4 overflow-hidden rounded shadow-lg">
       <img
         class="w-full"
         src="https://tailwindcss.com/img/card-top.jpg"
         alt="Sunset in the mountains"
       />
       <div class="px-6 py-4 pb-2">
-        <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
-        <p class="text-gray-700 text-base">
+        <div class="mb-2 text-xl font-bold">The Coldest Sunset</div>
+        <p class="text-base text-gray-700">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
           quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
           nihil.
         </p>
       </div>
       <div class="px-6 py-4">
-        <span class="badge mr-2">#photography</span>
-        <span class="badge mr-2">#travel</span>
+        <span class="mr-2 badge">#photography</span>
+        <span class="mr-2 badge">#travel</span>
         <span class="badge">#winter</span>
       </div>
     </div>
@@ -25,7 +25,7 @@
 
 <style lang="postcss" scoped>
 .badge {
-  @apply inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700;
+  @apply inline-block bg-gray-200 rounded-full px-3 py-1 text-base font-semibold text-gray-700;
   &:hover {
     @apply bg-gray-300;
   }
