195d3456a26ccecf44893ac92da4599f5c95d414
DESKTOP-P1UNOTJ\jason <1127lse@gmail.com>
2022-01-11 11:34:19 +0900

    feat: 디자인 가이드에 따른 transparent wrapper button component 생성(#19)

---
+++ components/button/formBtn.vue
@@ -0,0 +1,45 @@
+<template>
+  <button
+    v-on="$listeners"
+    type="button"
+    class="form-button"
+    :style="sizeObject"
+  >
+    {{ btnText }}
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    width: {
+      type: String,
+      default: () => "380",
+    },
+    height: {
+      type: String,
+      default: () => "56",
+    },
+    btnText: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      sizeObject: {
+        width: `${this.width}px`,
+        height: `${this.height}px`,
+      },
+    };
+  },
+};
+</script>
+
+<style lang="postcss" scoped>
+.form-button {
+  @apply px-4 py-2 text-center text-white 
+  transition duration-200 ease-in rounded-md
+  bg-orange1 hover:bg-orange2;
+}
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,17 @@
 module.exports = {
-  mode: 'jit',
-  purge: [
-    './pages/*.vue',
-  ],
+  mode: "jit",
+  purge: ["./pages/*.vue"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        orange1: "#F38C2B",
+        orange2: "#F3732B",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
