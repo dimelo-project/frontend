bd7d9a2f11371bc6528da3b8f69e2db5fe340ae0
silica67sdm <1127lse@gmail.com>
2022-01-12 15:48:47 +0900

    feat: login page 마크업 생성(#14)

---
+++ pages/login.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="flex justify-center mt-24 mb-24">
+    <div class="relative flex flex-col items-center test" style="width: 380px">
+      <h3 class="txt-heading3">로그인</h3>
+
+      <!-- ID (EMAIL) -->
+      <input
+        v-model="userId"
+        type="text"
+        class="w-full px-3 py-4 mt-8 border-2 rounded-md outline-none test"
+        :class="{ 'border-black': userId.length > 0 }"
+        placeholder="아이디(이메일)"
+        style="height: 56px"
+      />
+
+      <!-- PASSWORD -->
+      <input
+        v-model="userPassword"
+        type="password"
+        class="w-full px-3 py-4 border-2 rounded-md outline-none mt-7 test"
+        :class="{ 'password-dot-bigger border-black': userPassword.length > 0 }"
+        placeholder="비밀번호"
+        style="height: 56px"
+      />
+
+      <!-- error Msg box -->
+      <div class="flex items-center justify-start w-full h-8">
+        <p v-if="formError" class="text-red1 txt-sub">
+          아이디 또는 비밀번호가 일치하지 않습니다.
+        </p>
+      </div>
+
+      <!-- button -->
+      <ButtonGeneral btnText="로그인" />
+
+      <!-- sub link -->
+      <div class="relative flex w-full mt-7 test" style="height: 24px">
+        <p class="absolute pr-4 -translate-x-full txt-base left-1/2">
+          회원가입
+        </p>
+        <div
+          style="height: 24px"
+          class="absolute -translate-x-1/2 border left-1/2"
+        ></div>
+        <p class="absolute pl-4 txt-base left-1/2">비밀번호 찾기</p>
+      </div>
+
+      <!-- divider -->
+      <div class="w-full border mt-9"></div>
+
+      <!-- OAuth -->
+      <p class="mt-12">간편 로그인</p>
+
+      <!-- OAuth logo link-->
+      <div class="mt-4">
+        <div class="flex">
+          <NuxtLink class="mr-3" to="/">
+            <img src="~assets/imgs/logo/google_logo.png" alt="구글로그인" />
+          </NuxtLink>
+          <NuxtLink class="ml-3" to="/">
+            <img src="~assets/imgs/logo/github_logo.png" alt="깃헙로그인" />
+          </NuxtLink>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  layout: "home",
+  data() {
+    return {
+      userId: "",
+      userPassword: "",
+      formError: true,
+    };
+  },
+});
+</script>
+
+<style lang="postcss" scoped>
+.test {
+  /* @apply border-2 border-orange1; */
+}
+
+.password-dot-bigger {
+  font-family: Verdana;
+  letter-spacing: 0.125rem;
+}
+</style>
