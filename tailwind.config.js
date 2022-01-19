11888e90e2a5cd654020d5dc4aaea208e3a66924
DESKTOP-P1UNOTJ\jason <1127lse@gmail.com>
2022-01-19 20:44:52 +0900

    setting: tailwind style 속성 추가 (#36)

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,9 +11,24 @@ module.exports = {
         red1: "#E34B54",
         green1: "#49BC93",
         gray1: "#868E96",
+        gray2: "#E0E1E5",
+        gray3: "#F1F3F5",
       },
       spacing: {
+        15: "3.75rem",
         30: "120px",
+      },
+      margin: {
+        "1px": "1px",
+        "2px": "2px",
+        "3px": "3px",
+        "4px": "4px",
+        "5px": "5px",
+        "6px": "6px",
+        18: "4.5rem",
+      },
+      borderWidth: {
+        default: "1px",
       },
       borderRadius: {
         base: "4px",
