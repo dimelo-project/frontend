307da06790511a11ae51d05e269e1a3cf2ce829c
DESKTOP-P1UNOTJ\jason <1127lse@gmail.com>
2022-01-22 13:05:09 +0900

    feat: 평점정보를 가공하여 나온 수치에 따라 reviewStar 컴포넌트 개수조절(#50)

---
+++ components/starRate/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="flex">
+    <SvgReviewStar v-for="i in filledStarNum" :key="i" />
+    <SvgReviewStar :fill="false" v-for="i in unfilledStarNum" :key="i" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    score: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    filledStarNum() {
+      const score = Number(this.score);
+
+      if (score > 4.5 && score <= 5.0) {
+        return 5;
+      } else if (score > 3.5 && score <= 4.5) {
+        return 4;
+      } else if (score > 2.5 && score <= 3.5) {
+        return 3;
+      } else if (score > 1.5 && score <= 2.5) {
+        return 2;
+      } else {
+        return 1;
+      }
+    },
+    unfilledStarNum() {
+      return 5 - this.filledStarNum;
+    },
+  },
+  mounted() {
+    console.log(this.score);
+  },
+};
+</script>
