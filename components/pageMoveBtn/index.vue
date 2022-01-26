6c8a22a25dc681de198fa7fd11f0849c6388d51f
DESKTOP-P1UNOTJ\jason <1127lse@gmail.com>
2022-01-26 11:44:58 +0900

    feat: pageMoveBtn component 생성(#67)

---
+++ components/pageMoveBtn/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div
+    v-on="$listeners"
+    style="width: 50px; height: 50px"
+    class="flex items-center justify-center border rounded-full cursor-pointer border-gray2"
+  >
+    <img src="~/assets/imgs/icon/leftarrow.png" alt="뒤로가기" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {},
+};
+</script>
+
+<style lang="postcss" scoped></style>
