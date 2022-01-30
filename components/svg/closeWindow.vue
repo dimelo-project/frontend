6a1f30073a1a761b00b9005311c3c6da4dc79dfa
DESKTOP-P1UNOTJ\jason <1127lse@gmail.com>
2022-01-30 17:03:35 +0900

    feat: svg component 추가 & color props 설정 & transition css 설정(#78)

---
+++ components/svg/closeWindow.vue
@@ -0,0 +1,28 @@
+<template>
+  <svg
+    width="32"
+    height="32"
+    viewBox="0 0 32 32"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M1.54346 30.4559L30.4564 1.54297M1.54346 1.54297L30.4564 30.4559L1.54346 1.54297Z"
+      :stroke="color"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  props: {
+    color: {
+      type: String,
+      default: () => "black",
+    },
+  },
+};
+</script>
