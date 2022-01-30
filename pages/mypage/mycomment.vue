99874d5b4cb6376474933c45314dc9ccb382a37f
DESKTOP-P1UNOTJ\jason <1127lse@gmail.com>
2022-01-31 01:14:15 +0900

    feat: mypage mycomment page markup(#80)

---
+++ pages/mypage/mycomment.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <!-- divider -->
+    <div class="border-t-2 border-gray2"></div>
+
+    <!-- comment date list -->
+    <div v-for="i in 3" :key="i" class="mt-4">
+      <div>
+        <span>저도 참여하고 싶습니다!! 열심히 하고자 합니다!!ㅎㅎㅎㅎㅎㅇ</span>
+      </div>
+      <div class="txt-sub text-gray6">
+        <span>2021.11.23</span>
+      </div>
+      <div class="txt-sub text-rgay1">
+        <span>원글제목 말머리는빼고</span>
+      </div>
+      <div class="mt-4 border-t border-gray2"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: "mypage",
+};
+</script>
+
+<style lang="postcss" scoped></style>
