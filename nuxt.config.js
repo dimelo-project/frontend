f89a4baf692234f7a1f3eb57e6273066720a1a28
silica67sdm <1127lse@gmail.com>
2022-03-02 16:51:48 +0900

    setting: credential 제거

--- nuxt.config.js
+++ nuxt.config.js
@@ -82,7 +82,6 @@ export default {
           login: { url: "/api/auth/login", method: "post" },
           logout: { url: "/api/auth/logout", method: "post" },
           user: { url: "/api/users/me", method: "get", propertyName: false },
-          withCredentials: true,
         },
         tokenRequired: false,
         tokenType: false,
