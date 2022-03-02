13e43efa7fb916030c07b5c7bfcf909fbf96669f
silica67sdm <1127lse@gmail.com>
2022-03-03 01:00:26 +0900

    setting: proxy-server baseurl set

--- nuxt.config.js
+++ nuxt.config.js
@@ -52,7 +52,8 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: "https://dimeloserverapi.site",
+    baseURL:
+      "https://agile-beach-11445.herokuapp.com/http://dimeloserverapi.site",
     credentials: true,
     // common: {
     //   "Access-Control-Allow-Origin": "*",
@@ -109,9 +110,9 @@ export default {
     },
   },
 
-  proxy: {
-    "/api/": {
-      target: "https://dimeloserverapi.site",
-    },
-  },
+  // proxy: {
+  //   "/api/": {
+  //     target: "https://dimeloserverapi.site",
+  //   },
+  // },
 };
