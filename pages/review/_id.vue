217bb1023e3c04e122f1fc25999aff2930f257e8
DESKTOP-P1UNOTJ\jason <1127lse@gmail.com>
2022-03-20 19:10:24 +0900

    refactor: getLectureBrandLogo function global inject & plugin register(#287)

--- pages/review/_id.vue
+++ pages/review/_id.vue
@@ -6,7 +6,7 @@
         <!-- left content -->
         <div>
           <img
-            :src="cardImageLogo(courseData['course_platform'])"
+            :src="$getLectureBrandLogo(courseData['course_platform'])"
             alt="강의사이트로고"
             style="width: 69px; height: 69px"
             draggable="false"
@@ -478,31 +478,6 @@ export default {
     };
   },
   methods: {
-    cardImageLogo(siteName) {
-      let imgName = "";
-
-      if (siteName === "탈잉") {
-        imgName = "taling";
-      } else if (siteName === "인프런") {
-        imgName = "inflearn";
-      } else if (siteName === "유데미") {
-        imgName = "udemy";
-      } else if (siteName === "클래스101") {
-        imgName = "class101";
-      } else if (siteName === "리메인") {
-        imgName = "remain";
-      } else if (siteName === "패스트캠퍼스") {
-        imgName = "fastcampus";
-      } else if (siteName === "프로그래머스") {
-        imgName = "programmers";
-      } else if (siteName === "코드잇") {
-        imgName = "codeit";
-      } else {
-        imgName = "etc";
-      }
-
-      return require(`assets/imgs/logo/lecturesite/${imgName}.png`);
-    },
     async getFilteredReviewData(sortWay, orderWay, menuIdx) {
       try {
         const response = await this.$axios.$get(
